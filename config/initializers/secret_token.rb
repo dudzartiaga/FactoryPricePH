ec951bf7a1ce34551f60057a0e42e95249c2af70
Dudz Artiaga <dudzartiaga@gmail.com>
2013-10-15 22:02:29 -0700

    Code Origin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FactoryPricePH::Application.config.secret_key_base = '8874f034d51108a57adc79c6509c4a99c9f554d4902cd28e5caa87660d932745b1b1845d9a9fb534930487fd6d15d53bf3574ab071c6f0b2b1a9b83e34d47926'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FactoryPricePH::Application.config.session_store :cookie_store, key: '_FactoryPricePH_session'
